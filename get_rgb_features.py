56096d5f2bcf8a6349c482722fbe0c5051833266
mariyashcheg <mariyashcheg@gmail.com>
2020-03-15 16:10:04 +0300

    upd script & add resnet ft

--- get_rgb_features.py
+++ get_rgb_features.py
@@ -9,10 +9,12 @@ import torchvision
 import os
 import argparse
 import json
+import numpy as np
 
 from torch.utils.data.dataset import Dataset
 from torch.utils.data import DataLoader
-from tqdm import tqdm_notebook as tqdm
+from tqdm import tqdm
+from pathlib import Path
 
 
 FRAMES_PER_SEC = 5
@@ -35,32 +37,46 @@ class DiDeMoDataset(Dataset):
     def __getitem__(self, index):
         filename = self.dataset_info[index]['video']
         num_segments = self.dataset_info[index]['num_segments']
-
         vframes, _, vinfo = torchvision.io.read_video(
             filename='{}/{}.mp4'.format(self.dataset_dir, filename),
             pts_unit='sec',
             end_pts=SEC_PER_SEGMENT*num_segments
         )
-        # subsample frames (5 frames corresponds to 1 sec of the video)
-        step = vinfo['video_fps'] / FRAMES_PER_SEC
-        mask = torch.Tensor([round(i * step) for i in range(SELECT_FPS * (num_segments - 1))]).long()
-        last_frame = mask[-1].item()
-        step = (vframes.size(0) - last_frame - 1) / SELECT_FPS
-        mask = torch.cat([
-            mask,
-            torch.Tensor([last_frame + round((i + 1) * step) for i in range(SELECT_FPS)]).long()
-        ])
-        vframes = vframes.index_select(dim=0, index=mask)
+        if vframes.size(0) > 0: # successed to read videofile:
+            
+            video_length = vframes.size(0) / vinfo['video_fps']
+            # subsample frames (5 frames corresponds to 1 sec of the video)
+            if round(video_length) == num_segments*SEC_PER_SEGMENT: # all segments are full (5 sec each)
+                step = (vframes.size(0)-1) / (SELECT_FPS*num_segments+1)
+            else: # last segment is less than 5 sec
+                step = (round((num_segments-1)*SEC_PER_SEGMENT*vinfo['video_fps'])-1) / (SELECT_FPS * (num_segments-1)+1)
 
-        # [T, H, W, C] -> [T, C, H, W]
-        vframes = vframes.transpose(3, 1).transpose(2, 3)
+            stop = min(vframes.size(0), SELECT_FPS * num_segments * step)-1
+            curr_step = 0
+            mask = [curr_step]
+            while curr_step <= stop - step:
+                curr_step += step
+                mask.append(int(round(curr_step)))
+            mask = torch.Tensor(mask).long()
+            vframes = vframes.index_select(dim=0, index=mask)
 
-        # scale to [0, 1]
-        vframes = vframes.float().div(255)
-        # normalize with ImageNet mean and std
-        vframes = vframes.sub(self.mean[None, :, None, None]).div(self.std[None, :, None, None])
+            # [T, H, W, C] -> [T, C, H, W]
+            vframes = vframes.transpose(3, 1).transpose(2, 3)
 
-        return (vframes, filename)
+            # scale to [0, 1]
+            vframes = vframes.float().div(255)
+            # normalize with ImageNet mean and std
+            vframes = vframes.sub(self.mean[None, :, None, None]).div(self.std[None, :, None, None])
+
+            return dict(
+                frames=vframes, 
+                video=filename)
+
+        else:
+            return dict(
+                frames=torch.empty((0,1,1,3)),
+                video=filename)
+
 
     def __len__(self):
         return len(self.dataset_info)
@@ -72,10 +88,12 @@ if __name__ == '__main__':
     parser.add_argument("--dataset_dir", type=str, default='videos/')
     parser.add_argument("--features_dir", type=str, default='features/')
     parser.add_argument("--device", type=str, default=None)
+    parser.add_argument("--model", type=str, default='vgg19')
     args = parser.parse_args()
 
     DATASET_DIRECTORY = args.dataset_dir
     FT_DIRECTORY = args.features_dir
+    MODEL_TYPE = args.model
     if args.device in ['cpu','cuda']:
         DEVICE = args.device
     else:
@@ -84,22 +102,55 @@ if __name__ == '__main__':
         else:
             DEVICE = 'cpu'
 
-    video_filenames = [f[:-4] for f in os.listdir(DATASET_DIRECTORY)
-                            if os.path.isfile(os.path.join(DATASET_DIRECTORY, f)) and (f[-4:] == '.mp4')]
-
-    dataset_info = read_json('didemo_video_info.json')
-
+    Path(FT_DIRECTORY).mkdir(exist_ok=True)
+    done_videos = [f.stem[len('{}_ft_'.format(MODEL_TYPE)):] for f in
+            sorted(Path(FT_DIRECTORY).glob("{}_ft_*.npy".format(MODEL_TYPE)))]
+    if Path('missed_videos_features.json').exists():
+        with open('missed_videos_features.json', 'r') as f:
+            missed_videos = json.load(f)
+    else:
+        missed_videos = []
+    done_videos.extend(missed_videos)
+        
+    dataset_info = read_json('didemo_video_info.json') # [:len(done_videos)+4]
+    dataset_info = [data for data in dataset_info if data['video'] not in done_videos]
+    
     # read all videos from DiDeMo dataset
     didemo_dataset = DiDeMoDataset(dataset_info, dataset_dir=DATASET_DIRECTORY)
-    dataloader = DataLoader(didemo_dataset, batch_size=1, shuffle=False, num_workers=4)
+    dataloader = DataLoader(didemo_dataset, batch_size=1, shuffle=False, num_workers=1)
+    
+    if MODEL_TYPE == 'vgg19':
+        # load pretrained VGG19 net
+        model = torchvision.models.vgg19(pretrained=True).to(DEVICE)
+        # need features from fc7 (second FC-layer in classifier)
+        model.classifier = nn.Sequential(*[model.classifier[i] for i in range(5)])
+    elif MODEL_TYPE == 'resnet152':
+        # load pretrained ResNet152
+        model = torchvision.models.resnet152(pretrained=True)
+        # need features after avgpool layer (before final FC-head)
+        model = nn.Sequential(*(list(model.children())[:-1]))
 
-    # load pretrained VGG19 net
-    model_vgg19 = torchvision.models.vgg13(pretrained=True).to(DEVICE)
-
-    # need features from fc7 (second FC-layer in classifier)
-    model_vgg19.classifier = nn.Sequential(*[model_vgg19.classifier[i] for i in range(5)])
-
-    model_vgg19.eval()
-    for (vframes, video) in tqdm(dataloader):
-        features = model_vgg19(vframes.squeeze(0).to(DEVICE))
-        torch.save(features.detach().cpu(), '{}/vgg19_ft_{}.pt'.format(FT_DIRECTORY, video))
+    # missed_videos = []
+    batch_size = 16
+    model.eval()
+    for data in tqdm(dataloader):
+        vframes = data['frames']
+        video = data['video'][0]
+        if vframes.size(1) > 0: # successed to read videofile    
+            # video is a tuple (string, ), so..
+            # TODO(maria): dataloader could return dict, so try it
+            with torch.no_grad():
+                all_frames = vframes.squeeze(0)
+                features = []
+                for i in range(0, all_frames.shape[0], batch_size):
+                    x = all_frames[i: i + batch_size, ...].to(DEVICE)
+                    y = model(x).cpu().numpy()
+                    features.append(y)
+                # join features back
+                features = np.concatenate(features, axis=0)
+                np.save(Path(FT_DIRECTORY).joinpath('{}_ft_{}'.format(MODEL_TYPE, video)), features)
+        else:
+            missed_videos.append(video)
+    
+    with open('missed_videos_features.json', 'w') as f:
+        json.dump(missed_videos, f)
